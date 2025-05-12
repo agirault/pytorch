9d41b7016786540609eb438bac55a867a05a3ff0
James Wu <jjwu@meta.com>
2025-05-12 12:29:23 -0700

    Add justknobs for static cuda launcher
    Summary:
    This diff adds a justknobs check for static cuda launcher. In particular, it supports a fractional rollout where each mast job/version can be consistently enrolled in the config on or off. 
    
    It also adds a set_feature_use so we can track whether static cuda launcher is enabled on a given dynamo compile.
    
    Test Plan: Existing unit tests. The justknobs in question are set to be disabled right now, so this diff does not launch the feature yet.
    
    Differential Revision: D74599203

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -32,6 +32,7 @@ from typing import (
 import torch
 from torch._prims_common import compute_required_storage_length
 from torch.utils._ordered_set import OrderedSet
+from torch._dynamo.utils import set_feature_use
 
 from ..triton_bundler import TritonBundler
 from ..utils import prefix_is_reduction, triton_version_uses_attrs_dict
@@ -1288,6 +1289,9 @@ class StaticTritonCompileResult(CompileResult[StaticallyLaunchedCudaKernel]):
         self.kernel.cubin_path = cubin_location
 
     def make_launcher(self) -> LauncherType:
+        # If at least one static make_launcher call occurs,
+        # we're sure static cuda launcher was used for this compile
+        set_feature_use("static_cuda_launcher", True)
         # Load the binary on the parent
         if not self.kernel.cubin_path:
             self.reload_cubin_path()
